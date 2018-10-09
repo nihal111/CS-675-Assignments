80584f73c1c4fc54a435622270d9c4bc7a8601d3
Nihal Singh <nihal.111@gmail.com>
2018-10-09 19:09:29 +0530

    Fixed ortho

--- main.cpp
+++ main.cpp
@@ -348,7 +348,7 @@ void renderGL(void)
     projection_matrix = glm::frustum(-1.0, 1.0, -1.0, 1.0, 1.0, 10.0);
     // projection_matrix = glm::perspective(glm::radians(90.0),1.0,0.1,10.0);
   else
-    projection_matrix = glm::ortho(-2.0, 2.0, -2.0, 2.0, -5.0, 10.0);
+    projection_matrix = glm::ortho(-3.0, 3.0, -3.0, 3.0, 1.0, 10.0);
 
   view_matrix = projection_matrix*lookat_matrix;
 
