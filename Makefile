26cf08909b82ea214aa1e89215facd85bab7527c
Nihal Singh <nihal.111@gmail.com>
2018-10-15 10:46:01 +0530

    Just cloned assignment 2

---
+++ Makefile
@@ -0,0 +1,20 @@
+CC=g++
+
+OPENGLLIB= -lGL
+GLEWLIB= -lGLEW
+GLFWLIB = -lglfw
+LIBS=$(OPENGLLIB) $(GLEWLIB) $(GLFWLIB)
+LDFLAGS=-L/usr/local/lib 
+CPPFLAGS=-I/usr/local/include -I./
+
+BIN=main
+SRCS=main.cpp gl_framework.cpp shader_util.cpp hierarchy_node.cpp
+INCLUDES=gl_framework.hpp shader_util.hpp main.hpp hierarchy_node.hpp box.cpp ellipsoid.cpp opening_box.cpp cylinder.cpp
+
+all: $(BIN)
+
+$(BIN): $(SRCS) $(INCLUDES)
+	g++ $(CPPFLAGS) $(SRCS) -o $(BIN) $(LDFLAGS) $(LIBS)
+
+clean:
+	rm -f *~ *.o $(BIN)
---
+++ box.cpp
@@ -0,0 +1,41 @@
+#include "glm/ext.hpp"
+// quad generates two triangles for each face and assigns colors to the vertices
+int box_quad(int a, int b, int c, int d, glm::vec4* box_vertices, glm::vec4 color, int tri_idx, 
+  glm::vec4* box_positions, glm::vec4* box_colors)
+{
+  box_colors[tri_idx] = color; box_positions[tri_idx] = box_vertices[a];
+  tri_idx++;
+  box_colors[tri_idx] = color; box_positions[tri_idx] = box_vertices[b];
+  tri_idx++;
+  box_colors[tri_idx] = color; box_positions[tri_idx] = box_vertices[c];
+  tri_idx++;
+  box_colors[tri_idx] = color; box_positions[tri_idx] = box_vertices[a];
+  tri_idx++;
+  box_colors[tri_idx] = color; box_positions[tri_idx] = box_vertices[c];
+  tri_idx++;
+  box_colors[tri_idx] = color; box_positions[tri_idx] = box_vertices[d];
+  tri_idx++;
+  return tri_idx;
+ }
+
+csX75::HNode* get_box(glm::vec4* a_vertices, glm::vec4 a_color,
+                      GLfloat _min_rx=-1, GLfloat _max_rx=-1, 
+                      GLfloat _min_ry=-1, GLfloat _max_ry=-1, 
+                      GLfloat _min_rz=-1, GLfloat _max_rz=-1) {
+  int box_num_vertices = 36;
+  int tri_id=0;
+  glm::vec4 box_positions[36];
+  glm::vec4 box_colors[36];
+
+  tri_id = box_quad( 1, 0, 3, 2, a_vertices, a_color, tri_id, box_positions, box_colors);
+  tri_id = box_quad( 2, 3, 7, 6, a_vertices, a_color, tri_id, box_positions, box_colors);
+  tri_id = box_quad( 3, 0, 4, 7, a_vertices, a_color, tri_id, box_positions, box_colors);
+  tri_id = box_quad( 6, 5, 1, 2, a_vertices, a_color, tri_id, box_positions, box_colors);
+  tri_id = box_quad( 4, 5, 6, 7, a_vertices, a_color, tri_id, box_positions, box_colors);
+  tri_id = box_quad( 5, 4, 0, 1, a_vertices, a_color, tri_id, box_positions, box_colors);
+
+  csX75::HNode* box = new csX75::HNode(NULL, box_num_vertices, box_positions, box_colors, 
+                  sizeof(box_positions), sizeof(box_colors), 
+                  _min_rx, _max_rx, _min_ry, _max_ry, _min_rz, _max_rz);
+  return box;
+}
